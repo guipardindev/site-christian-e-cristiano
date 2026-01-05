d67b369fe8ea6c42e9d878d7f9a1a7d25115ed44
guipardindev <guipardindev@gmail.com>
2026-01-05 16:43:05 -0300

    commit add slider

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function toggleMenu() {
   document.getElementById('sideMenu').classList.toggle('active');
   document.getElementById('overlay').classList.toggle('active');
-}
+};
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 10000)
+
+function nextImage() {
+  count++;
+  if(count>2){
+    count = 1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+
+};
